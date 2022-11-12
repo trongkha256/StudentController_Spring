0d70842569055c829b6e1c02d2550a36983a2c15
trongkha256 <trongkha256@gmail.com>
2022-11-12 10:07:42 +0700

    feat: update src

---
+++ src/main/java/com/cybersoft/StudentController/StudentControllerApplication.java
@@ -0,0 +1,13 @@
+package com.cybersoft.StudentController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentControllerApplication.class, args);
+	}
+
+}
